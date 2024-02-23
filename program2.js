0ff7c71df1f22db3a7f41eeb5734737fa28351e6
karankumar2102 <126103261+karankumar2102@users.noreply.github.com>
2024-02-23 13:38:07 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,31 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanNumerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let result = 0;
 
-module.exports={romanToInt}
+    for (let i = 0; i < s.length; i++) {
+        const currentVal = romanNumerals[s[i]];
+        const nextVal = romanNumerals[s[i + 1]];
+
+        if (nextVal && currentVal < nextVal) {
+            result += nextVal - currentVal;
+            i++;
+        } else {
+            result += currentVal;
+        }
+    }
+
+    return result;
+}
+
+console.log(romanToInt("III")); 
+console.log(romanToInt("LVIII")); 
+console.log(romanToInt("MCMXCIV"));
