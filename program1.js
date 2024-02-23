ce6bd7b7890438b614b3633d65aeff6b9dd55b01
karankumar2102 <126103261+karankumar2102@users.noreply.github.com>
2024-02-23 13:36:54 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,27 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isValid(s) {
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
 
-module.exports = { isValid };
+    for (let char of s) {
+        if (pairs[char]) {
+            stack.push(char);
+        } else {
+            const openBracket = stack.pop();
+            if (pairs[openBracket] !== char) {
+                return false;
+            }
+        }
+    }
 
+    return stack.length === 0;
+}
 
+console.log(isValid("()")); 
+console.log(isValid("()[]{}")); 
+console.log(isValid("(]"));
+console.log(isValid("([)]")); 
+console.log(isValid("{[]}")); 
